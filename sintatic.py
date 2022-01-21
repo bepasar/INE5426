76b7438cf1732740f938b9e2d69ecd79e5d8c5f8
KlausDornsbach <klausdornsbach@gmail.com>
2022-01-21 11:05:26 -0300

    avancos no sintatico e readme

--- sintatic.py
+++ sintatic.py
@@ -1,11 +1,19 @@
 import ply.yacc as yacc
 
-from lexer import tokens
+from lexer import tokens, Lexer
 
 def p_program_statement(p):
     'program : statement'
     p[0] = p[1]
-    
+
+def p_statement_ident(p):
+    'statement : ident'
+    p[0] = p[1]
+
+def p_error(p):
+    print("Syntax error in input!")
+
+'''    
 def p_program_funclist(p):
     'program : funclist'
     p[0] = p[1]
@@ -17,4 +25,16 @@ def p_numexpression_term(p):
 def p_numexpression_plus(p):
     'numexpression : term PLUS'
     p[0] = p[1]
+'''
+lex = Lexer()
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = input('sintatic>')
+    except EOFError:
+        break
+    if not s: continue
+    result = parser.parse(input=s, debug=True)
+    print(result)
     
